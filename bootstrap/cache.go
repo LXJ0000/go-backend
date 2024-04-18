9a9d5866475d88076bfa3f4e509cc181f4ec04e7
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-04-18 22:05:50 +0800

    feat: kafka consumer async

--- bootstrap/cache.go
+++ bootstrap/cache.go
@@ -3,6 +3,8 @@ package bootstrap
 import (
 	_redis "github.com/LXJ0000/go-backend/redis"
 	"github.com/redis/go-redis/v9"
+	"golang.org/x/net/context"
+	"log"
 	"time"
 )
 
@@ -10,5 +12,8 @@ func NewRedisCache(env *Env) _redis.Cache {
 	cmd := redis.NewClient(&redis.Options{
 		Addr: env.RedisAddr,
 	})
+	if _, err := cmd.Ping(context.Background()).Result(); err != nil {
+		log.Fatal(err)
+	}
 	return _redis.NewCache(cmd, time.Duration(env.RedisExpiration)*time.Minute)
 }
