9f73ac78e9658681b4b5b547a7091e1a42441419
linxiaojiang <202100203017@stumail.sztu.edu.cn>
2024-04-22 10:31:51 +0800

    feat: prometheus

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"github.com/LXJ0000/go-backend/api/middleware"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+	"strconv"
 	"time"
 
 	route "github.com/LXJ0000/go-backend/api/route"
@@ -26,7 +30,49 @@ func main() {
 	server := gin.Default()
 	server.Use(middleware.CORSMiddleware())
 	server.Use(middleware.RateLimitMiddleware())
+	server.Use(ResponseTimeMiddleware())
 	route.Setup(env, timeout, db, cache, server, producer, saramaClient)
-
+	initPrometheus()
 	_ = server.Run(env.ServerAddress)
+}
+
+func initPrometheus() {
+	go func() {
+		// 专门给 prometheus 用的端口
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":8081", nil)
+	}()
+}
+
+func ResponseTimeMiddleware() gin.HandlerFunc {
+	labels := []string{"method", "pattern", "status"}
+	vector := prometheus.NewSummaryVec(prometheus.SummaryOpts{
+		Namespace: "jannan",
+		Subsystem: "go_backend",
+		Help:      "response time",
+		Name:      "gin_http_resp_time",
+		ConstLabels: map[string]string{
+			"instance_id": "",
+		},
+		Objectives: map[float64]float64{
+			0.5:   0.01,
+			0.75:  0.01,
+			0.9:   0.01,
+			0.99:  0.001,
+			0.999: 0.0001,
+		},
+	}, labels)
+	prometheus.MustRegister(vector)
+	return func(ctx *gin.Context) {
+		start := time.Now()
+		defer func() {
+			duration := time.Since(start).Milliseconds()
+			method := ctx.Request.Method
+			pattern := ctx.FullPath()
+			status := ctx.Writer.Status()
+			vector.WithLabelValues(method, pattern, strconv.Itoa(status)).
+				Observe(float64(duration))
+		}()
+		ctx.Next()
+	}
 }
